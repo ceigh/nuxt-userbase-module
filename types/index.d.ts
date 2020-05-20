5280bb44e5592ccd077f306eda8338bb2ffc7bf6
Artjom Löbsack <ceigh@pm.me>
2020-05-21 01:58:32 +0500

    :label: Add Session interface

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,27 @@
+interface User {
+  readonly username: string
+  readonly userId: string
+  readonly authToken: string
+  readonly creationDate: Date
+  readonly email?: string
+  readonly profile?: object
+  readonly paymentsMode: 'disabled' | 'test' | 'prod'
+  readonly subscriptionStatus?:
+    | 'active'
+    | 'incomplete'
+    | 'incomplete_expired'
+    | 'past_due'
+    | 'canceled'
+    | 'unpaid'
+    | 'trialing'
+  readonly trialExpirationDate?: Date
+  readonly cancelSubscriptionAt?: Date
+}
+interface Session {
+  readonly user?: User
+  readonly lastUsedUsername?: string
+}
+
 interface InitOpts {
   readonly appId: string
 }
@@ -5,5 +29,4 @@ interface InitOpts {
 interface Userbase {
   readonly init: (opts: InitOpts) => Promise<Session>
 }
-
 export type UserbaseInit = () => Userbase
